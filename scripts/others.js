3d1abe0ea307b10ced58160e35f0f3b7a6e4cb99
limcai21 <80958690+limcai21@users.noreply.github.com>
2022-08-02 22:24:06 +0800

    Add files via upload

---
+++ scripts/others.js
@@ -0,0 +1,30 @@
+function openNav() {
+    document.getElementById("sideNavBar").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("sideNavBar").style.width = "0px";
+}
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+window.onscroll = function() {progressBar()};
+
+function progressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
